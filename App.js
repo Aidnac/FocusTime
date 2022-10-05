da1d3463e5b6e419db13b983a5ceaf2f4d363a80
Can-A <calex6658@gmail.com>
2022-10-05 04:16:46 +0300

    Light weight navigation

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {Text, StyleSheet, View} from 'react-native';
 
 const App = () => {
-  return <Text>Hello world</Text>;
+  const [focusSubject, setFocusSubject] = useState('Hello world');
+
+  return (
+    <View style={styles.container}>
+      {focusSubject ? (
+        <Text>Here is where we r going to build the timer</Text>
+      ) : (
+        <Text>Here I want to build the input for a subject</Text>
+      )}
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 50,
+  },
+});
 
 export default App;
