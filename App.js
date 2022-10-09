162dc4414c2ada100c93729430f0d154f7f5064d
Can-A <calex6658@gmail.com>
2022-10-09 18:04:08 +0300

    Storing focus history

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, StyleSheet, View} from 'react-native';
 import {Focus} from './src/features/focus/Focus';
 import {Timer} from './src/features/timer/Timer';
 
+const STATUSES = {
+  COMPLETE: 1,
+  CANCELLED: 2,
+};
 const App = () => {
   const [focusSubject, setFocusSubject] = useState(null);
+  const [focusHistory, setFocusHistory] = useState([]);
+
+  const addFocusHistorySubjectWitthState = (subject, status) => {
+    setFocusHistory([...focusHistory, {subject, status}]);
+  };
+
+  console.log(focusHistory);
 
   return (
     <View style={styles.container}>
@@ -12,9 +23,13 @@ const App = () => {
         <Timer
           focusSubject={focusSubject}
           onTimerEnd={() => {
+            addFocusHistorySubjectWitthState(focusSubject, STATUSES.COMPLETE);
             setFocusSubject(null);
           }}
-          clearSubject={() => setFocusSubject(null)}
+          clearSubject={() => {
+            addFocusHistorySubjectWitthState(focusSubject, STATUSES.CANCELLED);
+            setFocusSubject(null);
+          }}
         />
       ) : (
         <Focus addSubject={setFocusSubject} />
